d25d95548a0dbd2dffdd708641bfe4b2f647b60e
Paulo Campos <paulo4-0@outlook.com>
2025-11-30 14:16:05 -0300

    add  and  files

---
+++ infra/database.js
@@ -0,0 +1,17 @@
+import { Client } from 'pg';
+
+async function query(queryObject) {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log('Database query result:', result.rows);
   res.status(200).json({ message: 'OK' });
 }
 
