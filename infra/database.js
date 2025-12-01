6be2566ff8df28fe59b21bc94ad944ac3d83f23b
Paulo Campos <paulo4-0@outlook.com>
2025-12-01 08:59:44 -0300

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -4,12 +4,16 @@ async function query(queryObject) {
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
   });
-
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
 
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error('Database query error:', error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
